02b33a2cc0a52858e772be2ad3786079063246e1
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-04-02 18:19:03 -0300

    Showing image field.
    Signed-off-by: Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
    Signed-off-by: Thiago Nogueira <thiagonf10@gmail.com>

--- CodaMais/CodaMais/settings.py
+++ CodaMais/CodaMais/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media directory
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'images')
--- CodaMais/CodaMais/urls.py
+++ CodaMais/CodaMais/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
 from exercise.views import *
@@ -22,3 +24,7 @@ urlpatterns = [
     url(r'^exercise/$', list_exercises_not_deprecated, name='list_exercises_not_deprecated'),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

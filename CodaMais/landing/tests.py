ff5b986ad7d392ecfb35c6e6870639e0e400ce7a
Kairon Velozo <kairon333@gmail.com>
2017-04-08 09:29:19 -0300

    Adding unit test for showing landing page.
    Signed-off-by: Hugo Alves <hugo.asb96@gmail.com>
    Signed-off-by: Kairon Velozo <kairon333@gmail.com>

--- CodaMais/landing/tests.py
+++ CodaMais/landing/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test.client import RequestFactory
 
-# Create your tests here.
+from landing.views import home
+
+
+class LandingViewTest(TestCase):
+    factory = RequestFactory()
+
+    def test_if_landing_page_is_showing(self):
+        request = self.factory.get('/')
+        response = home(request)
+        self.assertEqual(response.status_code, 200)

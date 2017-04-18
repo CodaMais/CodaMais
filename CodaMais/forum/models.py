f687e0fa47e24823621e2ed908044a6cb4f67586
Thiago Nogueira <thiagonf10@gmail.com>
2017-04-17 23:48:55 -0300

    Creating topic models and constants.
    Signed-off-by: Thiago Nogueira <thiagonf10@gmail.com>
    Singed-off-by: igor-gme <igorgabriel515@gmail.com>

--- CodaMais/forum/models.py
+++ CodaMais/forum/models.py
@@ -1,3 +1,19 @@
+# Django.
 from django.db import models
 
-# Create your models here.
+# Import datetime class.
+from datetime import datetime
+
+# local Django.
+from exercise import constants
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=constants.MAX_LENGTH_TITLE)
+    subtilte = models.CharField(max_length=constants.MAX_LENGTH_SUBTITLE)
+    autor = models.CharField(max_length=constants.MAX_LENGTH_AUTHOR)
+    description = models.CharField(max_length=constants.MAX_LENGTH_DESCRIPTION)
+    date = models.DateField(("Data"), default=datetime.date.today)
+
+    def __str__(self):
+        return self.title

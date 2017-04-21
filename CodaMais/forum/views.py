cdaab2361fdd3b381ec0f7c34709543b293af5d6
igor-gme <igorgabriel515@gmail.com>
2017-04-21 12:06:04 -0300

    Showing name of topic author in forum.
    Signed-off-by: Thiago Nogueira <thiagonf10@gmail.com>
    Signed-off-by: igor-gme <igorgabriel515@gmail.com>

--- CodaMais/forum/views.py
+++ CodaMais/forum/views.py
@@ -1,9 +1,16 @@
 # Django.
 from django.shortcuts import render, redirect
+import logging
 
 # local Django.
 from forum.models import Topic
 from forum.forms import TopicForm
+from django.contrib.auth.decorators import login_required
+from user.models import User
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def list_all_topics(request):
@@ -17,11 +24,17 @@ def show_topic(request, id):
     return render(request, 'show_topic.html', {'topic': topic})
 
 
+@login_required(login_url='/')
 def create_topic(request):
-    form = TopicForm(request.POST or None)
 
+    form = TopicForm(request.POST or None)
+    # user = User.objects.get(username=username)
+    username = request.user.username
+    logger.info("user: " + username)
     if form.is_valid():
-        form.save()
+        post = form.save(commit=False)
+        post.autor = username
+        post.save()
         return redirect('list_all_topics')
     else:
         return render(request, 'new_topic.html', {'form': form})

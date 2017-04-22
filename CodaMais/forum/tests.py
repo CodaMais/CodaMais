5c94c195e60e19200e09836d5a7e87b45f7862f3
igor-gme <igorgabriel515@gmail.com>
2017-04-22 00:03:53 -0300

    Adding model tests and some views tests.
    Signed-off-by: Thiago Nogueira <thiagonf10@gmail.com>
    Signed-off-by: igor-gme <igorgabriel515@gmail.com>

--- CodaMais/forum/tests.py
+++ CodaMais/forum/tests.py
@@ -1,3 +1,71 @@
+# Django.
 from django.test import TestCase
+from django.test.client import RequestFactory
 
-# Create your tests here.
+# local Django.
+from forum import constants
+from forum.models import Topic
+from forum.views import list_all_topics, show_topic
+
+
+class TestTopicCreation(TestCase):
+    topic = Topic()
+
+    def setUp(self):
+        self.topic.title = 'Basic Topic'
+        self.topic.subtilte = 'How test in Django'
+        self.topic.autor = 'User'
+        self.topic.description = '<p>Text Basic Exercise.</p>'
+
+    def test_str_is_correct(self):
+        self.topic.save()
+        topic_database = Topic.objects.get(id=self.topic.id)
+        self.assertEqual(str(topic_database), str(self.topic))
+
+    def test_if_topic_is_saved_database(self):
+        self.topic.save()
+        topic_database = Topic.objects.get(id=self.topic.id)
+        self.assertEqual(topic_database, self.topic)
+
+    def test_topic_get_title(self):
+        self.topic.save()
+        topic_database = Topic.objects.get(id=self.topic.id)
+        self.assertEqual(topic_database.title, self.topic.title)
+
+    def test_topic_get_subtilte(self):
+        self.topic.save()
+        topic_database = Topic.objects.get(id=self.topic.id)
+        self.assertEqual(topic_database.subtilte, self.topic.subtilte)
+
+    def test_topic_get_autor(self):
+        self.topic.save()
+        topic_database = Topic.objects.get(id=self.topic.id)
+        self.assertEqual(topic_database.autor, self.topic.autor)
+
+    def test_topic_get_description(self):
+        self.topic.save()
+        topic_database = Topic.objects.get(id=self.topic.id)
+        self.assertEqual(topic_database.description, self.topic.description)
+
+
+class TestRequestTopic(TestCase):
+
+        topic = Topic()
+
+        def setUp(self):
+            self.topic.title = 'Basic Topic'
+            self.topic.subtilte = 'How test in Django'
+            self.topic.autor = 'User'
+            self.topic.description = '<p>Text Basic Exercise.</p>'
+            self.factory = RequestFactory()
+
+        def test_list_all_topics(self):
+            request = self.factory.get('/forum')
+            response = list_all_topics(request)
+            self.assertEqual(response.status_code, constants.REQUEST_SUCCEEDED)
+
+        def test_show_topic(self):
+            self.topic.save()
+            request = self.factory.get('/forum')
+            response = show_topic(request, self.topic.id)
+            self.assertEqual(response.status_code, constants.REQUEST_SUCCEEDED)

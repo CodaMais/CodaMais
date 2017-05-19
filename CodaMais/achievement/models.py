ffe31446a98b9a46e0ee334d5afd261c21eec2fd
AllanNobre <allanjefrey9@hotmail.com>
2017-05-19 20:14:41 -0300

    Populating the database of achievements automatically.
    Signed-off-by: Hugo Alves <hugo.asb96@gmail.com>
    Signed-off-by: AllanNobre <allanjefrey9@hotmail.com>

--- CodaMais/achievement/models.py
+++ CodaMais/achievement/models.py
@@ -6,6 +6,8 @@ from achievement import constants
 from user.models import User
 
 
+# Class: Achievement
+# The class represents the achievements that the user can gets.
 class Achievement(models.Model):
     name = models.CharField(max_length=constants.MAX_LENGTH_NAME)
 
@@ -14,7 +16,7 @@ class Achievement(models.Model):
     # Indicate which group of achievements the current achievement is related to.
     achievement_type = models.PositiveIntegerField(choices=constants.ACHIEVEMENT_TYPE)
 
-    # Indicate the minimum quantity of something to unlock the achievement.
+    # Indicate the minimum quantity of something to unlock the achievement, based on achievement's type.
     quantity = models.PositiveIntegerField()
 
     achievement_icon = models.ImageField(default=constants.ACHIEVEMENTE_IMAGE, editable=True)
@@ -23,7 +25,7 @@ class Achievement(models.Model):
         return self.name
 
 
-# Class: Achievement
+# Class: UserAchievement
 # The class represents the relationship between the achievement and the user.
 # The table can only be stored when the user gets the achievement.
 class UserAchievement(models.Model):

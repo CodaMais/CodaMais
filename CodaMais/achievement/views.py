651a24e62331fa51b2df83fe977950950a9a78cb
AllanNobre <allanjefrey9@hotmail.com>
2017-05-21 02:28:34 -0300

    Modulating achievements verify methods.
    Signed-off-by: Hugo Alves <hugo.asb96@gmail.com>
    Signed-off-by: AllanNobre <allanjefrey9@hotmail.com>

--- CodaMais/achievement/views.py
+++ CodaMais/achievement/views.py
@@ -9,14 +9,17 @@ from achievement.models import (
     Achievement, UserAchievement
 )
 
-logging.basicConfig(level=logging.INFO)
+# Loggers Config
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 # Find all achievements related to corrected exercises and determine if the current user
 # is able to unlock some of them.
 def verify_correct_exercise_achievement(user):
-    logger.info("Verifying correct exercise achievement!")
+    logger.debug("Verifying correct exercise achievement.")
+
     user_corrected_exercise_quantity = get_user_corrected_exercise_quantity(user)
 
     # List of achievements that have the same type(correct exercise).
@@ -26,30 +29,20 @@ def verify_correct_exercise_achievement(user):
     # The list of achievements must be ordered by the biggest quantity of correct exercises.
     correct_exercise_achievements_list = correct_exercise_achievements_list.order_by('-quantity')
 
-    has_achievement = False
-    for achievement in correct_exercise_achievements_list:
-        if user_corrected_exercise_quantity >= achievement.quantity:
-            logger.info("Verifying if user: " + user.username + " has achievement: " + achievement.name)
-            has_achievement = check_if_user_has_achievement(user, achievement)
+    # Verify in fact wich achievement of the specific type achievements list the user should get.
+    check_achievement_user_should_get(user, user_corrected_exercise_quantity, correct_exercise_achievements_list)
 
-            if has_achievement is False:
-                unlock_achievement(user, achievement)
-                break
-            else:
-                logger.info("The user already had unlocked this achievement!")
-                break
-
-        else:
-            # Nothing to do.
-            pass
-
-    logger.info("There is no achievement to unlock!")
+    logger.debug("Ending verifycation of correct exercise achievement.")
 
 
 # Count all completed exercises for the current user.
 def get_user_corrected_exercise_quantity(user):
+    assert user is not None, "User not logged in."
+
     user_corrected_exercise_quantity = 0
     user_corrected_exercise_quantity = UserExercise.objects.filter(user=user, status=True).count()
+
+    assert user_corrected_exercise_quantity >= 0, "Invalid number of user corrected exercises"
 
     return user_corrected_exercise_quantity
 
@@ -57,7 +50,8 @@ def get_user_corrected_exercise_quantity(user):
 # Find all achievements related to submited answers in topics and determine if the current user
 # is able to unlock some of them.
 def verify_submited_answers_achievement(user):
-    logger.info("Verifying submited answers achievement!")
+    logger.debug("Verifying submited answers achievement.")
+
     user_submited_answers_quantity = get_user_submited_answers_quantity(user)
 
     # List of achievements that have the same type(submited answers).
@@ -67,36 +61,29 @@ def verify_submited_answers_achievement(user):
     # The list of achievements must be ordered by the biggest quantity of correct exercises.
     submited_answers_achievements_list = submited_answers_achievements_list.order_by('-quantity')
 
-    has_achievement = False
-    for achievement in submited_answers_achievements_list:
-        if user_submited_answers_quantity >= achievement.quantity:
-            logger.info("Verifying if user: " + user.username + " has achievement: " + achievement.name)
-            has_achievement = check_if_user_has_achievement(user, achievement)
+    # Verify in fact wich achievement of the specific type achievements list the user should get.
+    check_achievement_user_should_get(user, user_submited_answers_quantity, submited_answers_achievements_list)
 
-            if has_achievement is False:
-                unlock_achievement(user, achievement)
-                break
-            else:
-                logger.info("The user already had unlocked this achievement!")
-                break
-
-        else:
-            # Nothing to do.
-            pass
-
-    logger.info("There is no achievement to unlock!")
+    logger.debug("Ending verifycation of submited answers achievement.")
 
 
 # Count all submited answers for the current user.
 def get_user_submited_answers_quantity(user):
+    assert user is not None, "User not logged in."
+
     user_submited_answers_quantity = 0
     user_submited_answers_quantity = Answer.objects.filter(user=user).count()
+
+    assert user_submited_answers_quantity >= 0, "Invalid number of user submited answers"
 
     return user_submited_answers_quantity
 
 
 # Verifies if exists a relationship between the current user and the current achievement in database.
 def check_if_user_has_achievement(user, achievement):
+    assert user is not None, "User not logged in."
+    assert achievement is not None, "Achievement not logged in."
+
     try:
         UserAchievement.objects.get(user=user, achievement=achievement)
         has_achievement = True
@@ -109,6 +96,37 @@ def check_if_user_has_achievement(user, achievement):
 # Unlock the achievement creating a relationship between the current user
 # and the current achievement in database.
 def unlock_achievement(user, achievement):
+    assert user is not None, "User not logged in."
+    assert achievement is not None, "Achievement not logged in."
+
     user_achievement = UserAchievement()
     user_achievement.update_or_creates(user, achievement)
+
     logger.info(user.username + " just unlocked achievement " + achievement.name)
+
+
+# Verify which achievement the user should get or not, based on the specific quantity of
+# something that the achievement is related.
+def check_achievement_user_should_get(user, specific_user_quantity, achievements_list):
+    assert user is not None, "User not logged in."
+    assert achievements_list is not None, "Doesn't exist achievements in this Achievements list"
+    assert specific_user_quantity >= 0, "Invalid number of specific user quantity."
+
+    has_achievement = False
+    for achievement in achievements_list:
+        if specific_user_quantity >= achievement.quantity:
+            logger.info("Verifying if user: " + user.username + " has achievement: " + achievement.name)
+            has_achievement = check_if_user_has_achievement(user, achievement)
+
+            if has_achievement is False:
+                unlock_achievement(user, achievement)
+                break
+            else:
+                logger.info("The user: " + user.username + " already had unlocked this achievement.")
+                break
+
+        else:
+            # Nothing to do.
+            pass
+
+    logger.info("There is no achievement to unlock for the user: " + user.username + ".")

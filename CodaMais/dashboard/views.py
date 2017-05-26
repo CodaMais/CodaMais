b1dd85a5614282d0ccfdf709757498bde5842a38
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2017-05-26 12:25:02 -0300

    Adding data chart structure.
    Signed-off-by: Kairon Velozo <kairon333@gmail.com>
    Signed-off-by: João Paulo Nunes Soares <paulonsoares@yahoo.com.br>

--- CodaMais/dashboard/views.py
+++ CodaMais/dashboard/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from django.http import JsonResponse
+
 
 @login_required()
 def dashboard(request):
 
     return render(request, 'dashboard.html')
+
+
+def user_exercise_chart(request):
+    data = {
+            'labels': ['20/JPN - 30/Jan', '20/Feb - 20/Feb', '20/Mar - 20/Mar', '20/Abr - 20/Abr', '20/Mai - 20/Mai'],
+            'series': [
+                [542, 443, 320, 600, 553],
+                [412, 243, 280, 580, 453]
+              ]
+          }
+
+    return JsonResponse(data)
--- CodaMais/static/general/javascript/dashboard.js
+++ CodaMais/static/general/javascript/dashboard.js
@@ -2,21 +2,18 @@
 
   $(document).ready(function() {
 
-    var data = {
-              labels: ['20/Jan - 30/Jan', '20/Feb - 20/Feb', '20/Mar - 20/Mar', '20/Abr - 20/Abr', '20/Mai - 20/Mai'],
-              series: [
-                [542, 443, 320, 600, 553],
-                [412, 243, 280, 580, 453]
-              ]
-            };
+    $.getJSON("/dashboard/userExerciseChart",function(data){
 
-            var options = {
-                seriesBarDistance: 20,
-                axisX: {
-                    showGrid: false
-                },
-                height: "245px"
-            };
+      var options = {
+          seriesBarDistance: 20,
+          axisX: {
+              showGrid: false
+          },
+          height: "245px"
+      };
 
-            Chartist.Bar('#exercisesMadeByUser', data, options);
+      Chartist.Bar('#exercisesMadeByUser', data, options);
+
+    });
+
   });

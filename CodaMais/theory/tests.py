ca10b5da8620285737bda70bc601b9b06e79f8ff
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-06-02 20:09:18 -0300

    Adding get_exercise_list_in_theory (TDD).
    Signed-off-by: Ronyell Henrique dos Santos <allanjefrey9@hotmail.com>
    Signed-off-by: Allan Nobre <allanjefrey9@hotmail.com>

--- CodaMais/theory/tests.py
+++ CodaMais/theory/tests.py
@@ -2,10 +2,18 @@
 from django.test import TestCase
 from django.test.client import RequestFactory
 
-# Local Django.
-from theory.views import list_all_theories, show_theory
+# local Django
+from theory import (
+    constants, views,
+)
+
+from exercise.models import (
+    Exercise, TestCaseExercise
+)
+
+from user.models import User
+
 from theory.models import Theory
-from . import constants
 
 
 class TestTheoryRegistration(TestCase):
@@ -18,7 +26,7 @@ class TestTheoryRegistration(TestCase):
 
     def test_str_is_correct(self):
         self.theory.save()
-        theory_data = theory_data = Theory.objects.get(id=self.theory.id)
+        theory_data = Theory.objects.get(id=self.theory.id)
         self.assertEqual(str(theory_data), str(self.theory))
 
 
@@ -33,11 +41,50 @@ class TestRequestRegistration(TestCase):
 
     def test_list_all_theories(self):
         request = self.factory.get('/theory/')
-        response = list_all_theories(request)
+        response = views.list_all_theories(request)
         self.assertEqual(response.status_code, constants.REQUEST_SUCCEEDED)
 
     def test_show_theory(self):
         self.theory.save()
         request = self.factory.get('/theory/')
-        response = show_theory(request, self.theory.id, self.theory.title)
+        response = views.show_theory(request, self.theory.id, self.theory.title)
         self.assertEqual(response.status_code, constants.REQUEST_SUCCEEDED)
+
+
+class TestExerciseTheory(TestCase):
+    theory = Theory()
+    exercise = Exercise()
+    test_case_exercise = TestCaseExercise()
+    user = User()
+
+    def setUp(self):
+        self.user.email = "user@user.com"
+        self.user.first_name = "TestUser"
+        self.user.username = "Username"
+        self.user.is_active = True
+        self.exercise.title = 'Basic Exercise'
+        self.exercise.category = 2
+        self.exercise.statement_question = '<p>Text Basic Exercise.</p>'
+        self.exercise.score = 10
+        self.exercise.deprecated = 0
+        self.test_case_exercise.input_exercise = "a\n"
+        self.test_case_exercise.output_exercise = "B\n"
+        self.theory.title = 'Vector'
+        self.theory.content = '<p>Theory about Vector.</p>'
+        self.factory = RequestFactory()
+        self.user.set_password('userpassword')
+        self.theory.save()
+        self.user.save()
+
+        self.test_case_exercise.exercise = self.exercise
+        self.test_case_exercise.save()
+
+    def test_exercise_list_in_theory(self):
+        self.exercise.theory = self.theory
+        self.exercise.save()
+
+        list_exercises = views.exercise_list_in_theory(self.theory)
+        number_exercises = len(list_exercises)
+        number_expected = 1
+
+        self.assertEqual(number_expected, number_exercises)

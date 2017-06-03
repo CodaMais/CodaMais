06c37869385e6e13c2418fbbfcd1d421ffb0527c
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2017-06-03 20:34:30 -0300

    Adding dashboard request test.
    Signed-off-by: Kairon Velozo <kairon333.gmail.com>
    Signed-off-by: João Paulo Nunes Soares <paulonsoares@yahoo.com.br>

--- CodaMais/dashboard/tests.py
+++ CodaMais/dashboard/tests.py
@@ -1,3 +1,31 @@
+# Django
 from django.test import TestCase
+from django.test.client import RequestFactory
 
-# Create your tests here.
+# local Django
+from user.models import (
+    User,
+)
+from dashboard.views import dashboard
+
+# RESPONSE CODES.
+REQUEST_SUCCEEDED = 200  # 200 is return with success response.
+
+
+class DashboardViewTest(TestCase):
+    user = User()
+
+    def setUp(self):
+        self.user.email = "user@user.com"
+        self.user.first_name = "TestUser"
+        self.user.username = "Username"
+        self.user.is_active = True
+        self.user.set_password('userpassword')
+        self.user.save()
+        self.factory = RequestFactory()
+
+    def test_show_dashboard(self):
+        request = self.factory.get('/dashboard/dashboard')
+        request.user = self.user
+        response = dashboard(request)
+        self.assertEqual(response.status_code, REQUEST_SUCCEEDED)
